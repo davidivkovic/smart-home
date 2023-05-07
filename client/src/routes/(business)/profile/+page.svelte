1278cf53e1c6c57aa93d629725ff12cf5f9420cb
Milica Drača <draca.milica10@gmail.com>
2023-05-07 03:39:39 +0200

    2FA dialog

---
+++ client/src/routes/(business)/profile/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import TwoFactorDialog from "$lib/components/profile/TwoFactorDialog.svelte"
+  import { openDialog } from "$lib/stores/appStore"
+
+  const open2FADialog = () => openDialog(TwoFactorDialog)
+  
+</script>
+
+<button on:click={open2FADialog} class="primary">Open 2FA dialog</button>
