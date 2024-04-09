d8bc2a5ef37fe75edf4cecfad82999be69e2c6b2
David Ivković <gorajkora@gmail.com>
2024-04-09 17:59:55 +0200

    Update 3-create-keystore.sh

--- scripts/3-create-keystore.sh
+++ scripts/3-create-keystore.sh
@@ -7,9 +7,8 @@
 keytool -importkeystore -srckeystore ./output/root/ca.p12 -srcstoretype PKCS12 -destkeystore ./output/keystores/keystore.p12 -deststoretype PKCS12 -storepass:file ./config/ca-store-pass.txt -srcstorepass:file ./config/ca-store-pass.txt -noprompt
 keytool -importkeystore -srckeystore ./output/intermediate/ca-intermediate-eu.p12 -srcstoretype PKCS12 -destkeystore ./output/keystores/keystore.p12 -deststoretype PKCS12 -storepass:file ./config/ca-store-pass.txt -srcstorepass:file ./config/ca-store-pass.txt -noprompt
 
+mkdir -p ../smart-home-pki/pki/stores
 
 # Move the keystore to the final application keystores directory.
 mv ./output/keystores/keystore.p12 ../smart-home-pki/pki/stores
 mv ./output/keystores/truststore.p12 ../smart-home-pki/pki/stores
-
-rm -rf ./output
